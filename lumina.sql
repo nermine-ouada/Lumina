d73cd15a4dc63e173f7d6d8b20720e595fae6f0b
nermine.ouada <nermine.ouada@gmail.com>
2024-03-23 21:38:38 +0100

    sql file update

--- lumina.sql
+++ lumina.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2024 at 03:25 AM
+-- Generation Time: Mar 23, 2024 at 09:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,10 +40,170 @@ CREATE TABLE `admin` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `admin`
+-- Table structure for table `demande`
 --
 
+CREATE TABLE `demande` (
+  `demande_id` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fiche_suivi`
+--
+
+CREATE TABLE `fiche_suivi` (
+  `fiche_suivi_id` varchar(255) NOT NULL,
+  `participant_id` varchar(255) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `etat_paiement` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formateur`
+--
+
+CREATE TABLE `formateur` (
+  `formateur_id` varchar(255) NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tel` varchar(255) NOT NULL,
+  `rib` varchar(255) NOT NULL,
+  `banque` varchar(255) NOT NULL,
+  `specialite` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formation`
+--
+
+CREATE TABLE `formation` (
+  `formation_id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `formation_catgeory_id` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formation_category`
+--
+
+CREATE TABLE `formation_category` (
+  `formation_category_id` varchar(255) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formation_progress`
+--
+
+CREATE TABLE `formation_progress` (
+  `fg_id` varchar(255) NOT NULL,
+  `formateur_id` varchar(255) NOT NULL,
+  `module_id` varchar(255) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module`
+--
+
+CREATE TABLE `module` (
+  `module_id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `formation_id` varchar(255) NOT NULL,
+  `volume_horaire` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participant`
+--
+
+CREATE TABLE `participant` (
+  `participant_id` varchar(255) NOT NULL,
+  `cin` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tel` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `profession` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promotion`
+--
+
+CREATE TABLE `promotion` (
+  `promotion_id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `taux_reduction` float NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE `session` (
+  `session_id` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `niveau` varchar(255) NOT NULL,
+  `formation_id` varchar(255) NOT NULL,
+  `promotion_id` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -56,6 +216,55 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`),
   ADD UNIQUE KEY `cin` (`cin`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `demande`
+--
+ALTER TABLE `demande`
+  ADD PRIMARY KEY (`demande_id`);
+
+--
+-- Indexes for table `fiche_suivi`
+--
+ALTER TABLE `fiche_suivi`
+  ADD PRIMARY KEY (`fiche_suivi_id`);
+
+--
+-- Indexes for table `formation_category`
+--
+ALTER TABLE `formation_category`
+  ADD PRIMARY KEY (`formation_category_id`);
+
+--
+-- Indexes for table `formation_progress`
+--
+ALTER TABLE `formation_progress`
+  ADD PRIMARY KEY (`fg_id`);
+
+--
+-- Indexes for table `module`
+--
+ALTER TABLE `module`
+  ADD PRIMARY KEY (`module_id`);
+
+--
+-- Indexes for table `participant`
+--
+ALTER TABLE `participant`
+  ADD PRIMARY KEY (`participant_id`),
+  ADD UNIQUE KEY `cin` (`cin`,`email`,`tel`);
+
+--
+-- Indexes for table `promotion`
+--
+ALTER TABLE `promotion`
+  ADD PRIMARY KEY (`promotion_id`);
+
+--
+-- Indexes for table `session`
+--
+ALTER TABLE `session`
+  ADD PRIMARY KEY (`session_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
