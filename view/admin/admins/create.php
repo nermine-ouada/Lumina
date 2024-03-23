33977abb7bab4c2a8a085c68821081dd3ceb96ad
nermine.ouada <nermine.ouada@gmail.com>
2024-03-23 02:20:54 +0100

    fixed admin form layout

--- view/admin/admins/create.php
+++ view/admin/admins/create.php
@@ -6,20 +6,50 @@
             <div class="card">
                 <div class="card-body">
                     <form>
+                        <div class="row">
+                            <div class="mb-3 w-50">
+                                <label for="exampleInputEmail1" class="form-label">First name</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                            </div>
+                            <div class="mb-3 w-50">
+                                <label for="exampleInputEmail1" class="form-label">Last name</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                            </div>
+                        </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1"
-                                aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <label for="exampleInputPassword1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1">
                         </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <div class="row">
+                            <div class="mb-3 w-50">
+                                <label for="exampleInputEmail1" class="form-label">Phone number</label>
+                                <input type="tel" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">Enter only your phone number +216 ** *** ***
+                                </div>
+                            </div>
+                            <div class="mb-3 w-50">
+                                <label for="exampleInputEmail1" class="form-label">CIN</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">Enter your 8 digit CIN number.
+                                </div>
+                            </div>
                         </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Poste</label>
+                            <Select class="form-control">
+                                <option value="">Manager</option>
+                                <option value="">HR</option>
+                            </Select>
+                        </div>
+                        
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
