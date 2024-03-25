3b6a62be57e64e95a06626791be53422c828174e
wiem-hichri <wiemhichri794@gmail.com>
2024-03-25 23:27:18 +0100

    dernier modéfications

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 $servername = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'Lumina';
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$dbname = 'lumina';
+$port='3308';
+$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
 
 ?>
