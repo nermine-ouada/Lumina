ccbcc5c459c053f125b0e263b090cdd226b48bfc
nermine.ouada <nermine.ouada@gmail.com>
2024-03-27 22:50:17 +0100

    fixed bugs

--- view/admin/participant_crud/create.php
+++ view/admin/participant_crud/create.php
@@ -12,67 +12,65 @@ include ('../layouts/header.php');
 
                     <div class="row">
                         <div class="mb-3 w-50">
-                            <label  class="form-label">First name</label>
+                            <label class="form-label">First name</label>
                             <input type="text" class="form-control" name="first_name">
                         </div>
                         <div class="mb-3 w-50">
-                            <label  class="form-label">Last name</label>
+                            <label class="form-label">Last name</label>
                             <input type="text" class="form-control" name="last_name">
                         </div>
                     </div>
                     <div class="mb-3">
-                        <label   class="form-label">Email address</label>
+                        <label class="form-label">Email address</label>
                         <input type="email" class="form-control" name="email">
                     </div>
                     <div class="mb-3">
-                        <label   class="form-label">Password</label>
+                        <label class="form-label">Password</label>
                         <input type="password" class="form-control" name="password">
                     </div>
                     <div class="row">
                         <div class="mb-3 w-50">
-                            <label  class="form-label">Phone number</label>
+                            <label class="form-label">Phone number</label>
                             <input type="tel" class="form-control" name="tel" aria-describedby="emailHelp">
-                            <div   class="form-text">Enter only your phone number +216 ** *** ***
+                            <div class="form-text">Enter only your phone number +216 ** *** ***
 
                             </div>
                         </div>
                         <div class="mb-3 w-50">
-                            <label  class="form-label">Adresse</label>
+                            <label class="form-label">Adresse</label>
                             <input type="text" class="form-control" name="address">
                         </div>
 
                     </div>
-            </div>
-            <div class="row">
+                    <div class="row">
 
-                <div class="mb-3  w-50">
-                    <label  class="form-label">CIN</label>
-                    <input type="text" class="form-control" name="cin" aria-describedby="emailHelp">
-                    <div   class="form-text">Enter your 8 digit CIN number.
+                        <div class="mb-3  w-50">
+                            <label class="form-label">CIN</label>
+                            <input type="text" class="form-control" name="cin" aria-describedby="emailHelp">
+                            <div class="form-text">Enter your 8 digit CIN number.
+                            </div>
+                        </div>
+
+                        <div class="mb-3 w-50">
+
+                            <label class="form-label">Profession</label>
+                            <Select name="profession" class="form-control">
+                                <option></option>
+                                <option value="Mobile App Developer">Mobile App Developer</option>
+                                <option value="Data Scientist">Data Scientist </option>
+                                <option value="UI/UX Designer">UI/UX Designer</option>
+                                <option value="Cloud Architect">Cloud Architect</option>
+                                <option value="Systems Administrator">Systems Administrator</option>
+                                <option value="Student">Student</option>
+                            </Select>
+                        </div>
                     </div>
-                </div>
-
-                <div class="mb-3 w-50">
-
-                    <label  class="form-label">Profession</label>
-                    <Select name="profession" class="form-control">
-                        <option></option>
-                        <option value="Mobile App Developer">Mobile App Developer</option>
-                        <option value="Data Scientist">Data Scientist </option>
-                        <option value="UI/UX Designer">UI/UX Designer</option>
-                        <option value="Cloud Architect">Cloud Architect</option>
-                        <option value="Systems Administrator">Systems Administrator</option>
-
-                    </Select>
-                </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+
         </div>
-
-
     </div>
-</div>
 
-<?php include ('../layouts/footer.php'); ?>
+    <?php include ('../layouts/footer.php'); ?>
