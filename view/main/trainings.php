07a1dbaf5413f96c4caaca3852a6a69e74979e59
nermine.ouada <nermine.ouada@gmail.com>
2024-03-28 02:46:13 +0100

    changed main page

---
+++ view/main/trainings.php
@@ -0,0 +1,16 @@
+<?php include ('header.php'); ?>
+<!-- Page Header Start -->
+<div class="container-fluid contact page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container">
+        <h1 class="display-3 mb-4 animated slideInDown">Trainings & Diversity</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="home.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Trainings</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<?php include ('footer.php'); ?>
