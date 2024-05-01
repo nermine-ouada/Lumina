abcca15c42615a4357cd3501d631f2b6fda56fae
nermine.ouada <nermine.ouada@gmail.com>
2024-05-01 15:29:40 +0100

    added suggestion

---
+++ view/formateur/formation_category_crud/delete.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['admin'])) {
+    header("location:../auth/login.html");
+}
+include ("../../../config.php");
+
+$req = $conn->prepare('delete from formation_category where formation_category_id=?');
+
+$req->execute([$_GET['formation_category_id']]);
+
+header('location:index.php');
+
+?>
