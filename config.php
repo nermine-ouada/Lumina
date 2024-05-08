84fc4d1ce24ca91dfae6e7e1d0b00468fb8e41b6
Nermine Ouada <122449372+nermine-ouada@users.noreply.github.com>
2024-05-08 15:35:41 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'Lumina';
-$port = '3306';
-$conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$port", $username, $password);
-
-?>
