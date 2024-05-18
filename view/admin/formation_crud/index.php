6f0036fb97118b2286af6fde2a1418375fe5b8bd
wiem-hichri <wiemhichri794@gmail.com>
2024-05-18 23:37:17 +0200

    last modification

--- view/admin/formation_crud/index.php
+++ view/admin/formation_crud/index.php
@@ -2,8 +2,10 @@
 session_start();
 
 if (!isset($_SESSION['admin'])) {
-    header("location:../auth/login.phpview/admin/layouts/message.php");
+    header("location:../auth/login.php");
+    exit();
 }
+
 include ('../layouts/header.php');
 require '../../../config.php';
 ?>
@@ -13,7 +15,7 @@ require '../../../config.php';
     <div class="row">
         <form action="index.php" method="GET">
             <div class="d-sm-flex d-block align-items-center justify-content mb-9">
-                <input type="text" class="form-control w-50" name="search" placeholder="Filter by search">
+                <input type="text" class="form-control w-50" name="search" placeholder="Filter by search" value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>">
                 <button type="submit" class="btn btn-outline-primary m-3">Filter</button>
                 <?php if (isset($_GET["search"])) { ?>
                     <a href="index.php" class="btn btn-danger m-3">Undo search</a>
@@ -28,7 +30,6 @@ require '../../../config.php';
                         <div class="mb-3 mb-sm-0">
                             <h5 class="card-title fw-semibold">Formation Table</h5>
                         </div>
-
                         <div>
                             <a href="create.php" class="btn btn-primary">Add new formation</a>
                         </div>
@@ -37,146 +38,82 @@ require '../../../config.php';
                         <table class="table text-nowrap mb-0 align-middle">
                             <thead class="text-dark fs-4">
                                 <tr>
-                                    <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">Title & category</h6>
-                                    </th>
-                                    <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">Description</h6>
-                                    </th>
-
-                                    <th class="border-bottom-0">
-                                    </th>
+                                    <th class="border-bottom-0"><h6 class="fw-semibold mb-0">Title & Category</h6></th>
+                                    <th class="border-bottom-0"><h6 class="fw-semibold mb-0">Description</h6></th>
+                                    <th class="border-bottom-0"></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
-                                if (!isset($_GET['search'])) {
-                                    $sql = 'SELECT title, description, category_name, formation_id FROM formation 
-                                    JOIN formation_category ON formation.formation_category_id = formation_category.formation_category_id';
+                                $sql = 'SELECT title, description, category_name, formation.formation_id 
+                                        FROM formation 
+                                        JOIN formation_category ON formation.formation_category_id = formation_category.formation_category_id';
+                                
+                                $params = [];
+                                if (isset($_GET['search'])) {
+                                    $search = '%' . $_GET["search"] . '%';
+                                    $sql .= ' WHERE title LIKE ? OR description LIKE ? OR category_name LIKE ?';
+                                    $params = [$search, $search, $search];
+                                }
 
-                                    // Prepare and execute the statement
-                                    $req = $conn->prepare($sql);
-                                    $req->execute();
-                                    if ($req->rowCount() == 0) {
-                                        ?>
-                                        <tr>
-                                            <td>No data in the table</td>
-                                        </tr>
-                                    <?php } ?>
+                                $req = $conn->prepare($sql);
+                                $req->execute($params);
 
-                                    <?php while ($row = $req->fetch()) {
-                                        $sql1 = 'SELECT count(*) as nb FROM module where formation_id=?';
+                                if ($req->rowCount() == 0) {
+                                    echo '<tr><td colspan="3">No data found</td></tr>';
+                                } else {
+                                    while ($row = $req->fetch()) {
+                                        $formation_id = $row["formation_id"];
+                                        $sql1 = 'SELECT count(*) as nb FROM module WHERE formation_id=?';
                                         $req2 = $conn->prepare($sql1);
-                                        $req2->execute([$row["formation_id"]]);
+                                        $req2->execute([$formation_id]);
                                         $nb = $req2->fetch();
+
+                                        $title = htmlspecialchars($row['title']);
+                                        $category_name = htmlspecialchars($row['category_name']);
+                                        $description = htmlspecialchars($row['description']);
                                         ?>
-
                                         <tr>
-
                                             <td class="border-bottom-0">
-                                                <h6 class="mb-0 fw-semibold">
-                                                    <?php $title = $row['title'];
-                                                    echo $row["title"] ?>
-                                                </h6> <span class="fw-normal"><?= $row["category_name"] ?></span>
-
+                                                <h6 class="mb-0 fw-semibold"><?= $title ?></h6>
+                                                <span class="fw-normal"><?= $category_name ?></span>
                                             </td>
                                             <td class="border-bottom-0">
-                                                <p class="mb-0 fw-normal">
-                                                    <?php echo $row["description"] ?>
-                                                </p>
+                                                <p class="mb-0 fw-normal"><?= $description ?></p>
                                             </td>
-
-
                                             <td class="border-bottom-0">
-                                                <a onclick="return confirm('Are you sure you want to add a new module to <?php echo $title ?>?')"
-                                                    href="../module_crud/create.php?formation_id=<?php echo $row["formation_id"] ?>"
+                                                <a onclick="return confirm('Are you sure you want to add a new module to <?= $title ?>?')"
+                                                    href="../module_crud/create.php?formation_id=<?= $formation_id ?>"
                                                     class="btn btn-primary m-1">Add module</a>
                                             </td>
-                                            <?php if ($nb["nb"] != 0) { ?>
-
-                                                <td class="border-bottom-0">
-                                                    <a onclick="return confirm('Are you sure you want to add  <?php echo $title ?> to a new session?')"
-                                                        href="../session_crud/create.php?formation_id=<?php echo $row["formation_id"] ?>"
-                                                        class="btn btn-outline-primary m-1">Add to a session</a>
-                                                </td>
-                                                <?php
-                                            } else { ?>
-                                                <td class="border-bottom-0">
-                                                       <a class="btn btn-outline-danger disabled m-1">Cannot add to session</a>
-                                                </td>
-                                            <?php
-                                            } ?>
                                             <td class="border-bottom-0">
-                                                <a href="edit.php?formation_id=<?php echo $row["formation_id"] ?>"
+                                                <?php if ($nb["nb"] != 0) { ?>
+                                                    <a onclick="return confirm('Are you sure you want to add <?= $title ?> to a new session?')"
+                                                        href="../session_crud/create.php?formation_id=<?= $formation_id ?>"
+                                                        class="btn btn-outline-primary m-1">Add to a session</a>
+                                                <?php } else { ?>
+                                                    <a class="btn btn-outline-danger disabled m-1">Cannot add to session</a>
+                                                <?php } ?>
+                                            </td>
+                                            <td class="border-bottom-0">
+                                                <a href="edit.php?formation_id=<?= $formation_id ?>"
                                                     class="btn btn-outline-warning m-1">Edit</a>
                                             </td>
                                             <td class="border-bottom-0">
                                                 <a onclick="return confirm('Are you sure you want to delete?')"
-                                                    href="delete.php?formation_id=<?php echo $row["formation_id"] ?>"
+                                                    href="delete.php?formation_id=<?= $formation_id ?>"
                                                     class="btn btn-outline-danger m-1">Delete</a>
                                             </td>
-
                                         </tr>
-                                    <?php }
-                                } else if (isset($_GET["search"])) {
-                                    $search = '%' . $_GET["search"] . '%';
-                                    $sql = 'SELECT title, description, category_name, formation_id FROM formation 
-                                            JOIN formation_category ON formation.formation_category_id = formation_category.formation_category_id  
-                                  WHERE title LIKE ? 
-                                        OR description LIKE ? 
-                                        OR category_name LIKE ? 
-                                        ';
-
-
-                                    $req = $conn->prepare($sql);
-                                    $req->execute([$search, $search, $search]);
-
-                                    if ($req->rowCount() == 0) {
-                                        ?>
-                                            <tr>
-                                                <td>No data found</td>
-                                            </tr>
-                                    <?php } ?>
-
-                                    <?php while ($row = $req->fetch()) { ?>
-                                            <tr>
-
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">
-                                                    <?php echo $row["title"] ?>
-                                                    </p>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">
-                                                    <?php echo $row["description"] ?>
-                                                    </p>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <p class="mb-0 fw-normal">
-                                                    <?php echo $row["category_name"] ?>
-                                                    </p>
-                                                </td>
-
-                                                <td class="border-bottom-0">
-                                                    <a href="edit.php?formation_id=<?php echo $row["formation_id"] ?>"
-                                                        class="btn btn-outline-warning m-1">Edit</a>
-                                                </td>
-                                                <td class="border-bottom-0">
-                                                    <a onclick="return confirm('Are you sure you want to delete?')"
-                                                        href="delete.php?formation_id=<?php echo $row["formation_id"] ?>"
-                                                        class="btn btn-outline-danger m-1">Delete</a>
-                                                </td>
-
-                                            </tr>
                                     <?php }
                                 } ?>
                             </tbody>
                         </table>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <?php include ('../layouts/footer.php'); ?>
