17f6fc870c81b6e66920e8fe0f18a148f6649b04
tesnim4 <tesnimrhaiem9@gmail.com>
2024-05-21 00:08:47 +0200

    Votre message de commit

--- view/admin/layouts/header.php
+++ view/admin/layouts/header.php
@@ -195,7 +195,7 @@
                                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
                                     aria-labelledby="drop2">
                                     <div class="message-body">
-                                        <a href="../layouts/profile1.php"
+                                        <a href="profile1.php"
                                             class="d-flex align-items-center gap-2 dropdown-item">
                                             <i class="ti ti-user fs-6"></i>
                                             <p class="mb-0 fs-3"><?php echo $_SESSION['username']; ?></p>
